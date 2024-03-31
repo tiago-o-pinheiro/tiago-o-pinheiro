483d015ad1700cdb91e0682dda780ead3fb84815
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-03-31 14:18:16 +0200

    [feat] Added footer

--- app/sections/Footer.tsx
+++ app/sections/Footer.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 export const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
   return (
     <div className="w-full mt-4 flex justify-center border-t divide-gray-300 py-4 dark:bg-gray-800">
-      <p className="dark:text-white text-gray">Footer</p>
+      <p className="dark:text-white text-gray text-sm">
+        © {year} - Made with ❤️ by Tiago Pinheiro |{" "}
+        <a href="mailto:tiagoliver@protonmail.com">tiagoliver@protonmail.com</a>
+      </p>
     </div>
   );
 };
