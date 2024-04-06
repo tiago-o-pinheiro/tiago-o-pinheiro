d97ba13bff871c42196a02a21e615f1179278a89
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-04-06 13:43:15 +0200

    [fix] Adjusted footer text position

--- app/sections/Footer.tsx
+++ app/sections/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
   const year = date.getFullYear();
   return (
     <div className="w-full mt-4 flex justify-center border-t divide-gray-300 py-4 dark:bg-gray-800">
-      <p className="dark:text-white text-gray text-sm">
+      <p className="dark:text-white text-gray text-sm text-center">
         © {year} - Made with ❤️ by Tiago Pinheiro |{" "}
         <a href="mailto:tiagoliver@protonmail.com">tiagoliver@protonmail.com</a>
       </p>
